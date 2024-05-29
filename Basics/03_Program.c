7d3bbbc25f15ed0ffc5fb78fbb8df4884b7a8ad9
Jaspreet <jaspreetmehra185@gmail.com>
2024-05-29 21:27:17 +0530

    find size of datatypes

---
+++ Basics/03_Program.c
@@ -0,0 +1,9 @@
+// Write a C program to find the size of int, float, double, and char.
+#include <stdio.h>
+
+int main(){
+    printf("Size of int: %d\n", sizeof(int));
+    printf("Size of float: %d\n", sizeof(float));
+    printf("Size of double: %d\n", sizeof(double));
+    printf("Size of char: %d\n", sizeof(char));
+}
