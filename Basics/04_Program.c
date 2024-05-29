dcc8bc7b5327ab2eb5e091f94583da68eb7eb3dd
Jaspreet <jaspreetmehra185@gmail.com>
2024-05-29 21:44:21 +0530

    swap two numbers

---
+++ Basics/04_Program.c
@@ -0,0 +1,11 @@
+// Write a C program to swap two numbers.
+#include <stdio.h>
+
+int main(){
+    int a = 2, b = 3, c;
+    c = a;
+    a = b;
+    printf("a = %d, b = %d", a, c);
+
+    return 0;
+}
